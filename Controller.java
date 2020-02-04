90cdb3d1cf1fcbffe08dbadf40e3a13111e9990f
Paquette <1037576@etu.cegep-lanaudiere.qc.ca>
2020-02-04 16:39:26 -0500

    Version 1 - Charles Paquette

---
+++ Controller.java
@@ -0,0 +1,60 @@
+package sample;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
+
+public class Controller {
+    public TextField txfSource;
+    public ImageView imvGarbage;
+    public ComboBox cmbAjoutTexte;
+
+    public void txfSourceDragDetected(MouseEvent mouseEvent) {
+        if (txfSource.getText() != ("")){
+            TextField ref = (TextField) mouseEvent.getSource();
+            Dragboard db = ref.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(ref.getText());
+            db.setContent(content);
+        }
+    }
+
+    public void imvTargetDragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    public void imvTargetDragDropped(DragEvent dragEvent) {
+        TextField refSource = (TextField) dragEvent.getGestureSource();
+        Dragboard db = dragEvent.getDragboard();
+        refSource.clear();
+    }
+
+    public void cmbTargetDragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    public void cmbTargetDragDropped(DragEvent dragEvent) {
+        TextField refSource = (TextField) dragEvent.getGestureSource();
+        ComboBox refTarget = (ComboBox) dragEvent.getGestureTarget();
+        Dragboard db = dragEvent.getDragboard();
+        refTarget.getItems().addAll(refSource.getText());
+        refTarget.getSelectionModel().selectLast();
+    }
+
+    public void txfTargetDragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    public void txfTargetDragDropped(DragEvent dragEvent) {
+        TextField refSource = (TextField) dragEvent.getGestureSource();
+        TextField refTarget = (TextField) dragEvent.getGestureTarget();
+        Dragboard db = dragEvent.getDragboard();
+        if (refSource != refTarget) {
+            refTarget.setText(refSource.getText());
+            refSource.clear();
+        }
+    }
+
+}
